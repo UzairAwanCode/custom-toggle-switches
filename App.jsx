4171dd6a1d3935b189b22724c29bd991aae958e5
UzairAwanCode <githubprojects000@gmail.com>
2024-05-21 16:10:46 +0500

    Custom Toggle Switches

--- App.jsx
+++ App.jsx
@@ -1,13 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { useState } from 'react';
-import { Alert, StyleSheet, Text, View } from 'react-native';
-import Toggle from 'react-native-toggle-input'
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import Toggle from "react-native-toggle-input";
 
 export default function App() {
-  const [toggle, setToggle] = useState(false)
+  const [toggle, setToggle] = useState(false);
   return (
     <View style={styles.container}>
-      <Toggle toggle={toggle} setToggle={setToggle} color={"orange"} size={20} filled={true} circleColor={"white"} onTrue={()=>Alert.alert("Switched On")} onFalse={()=>Alert.alert("Switched Off")}/>
+      <Toggle
+        toggle={toggle}
+        setToggle={setToggle}
+        color={"orange"}
+        size={20}
+        filled={true}
+        circleColor={"white"}
+        onTrue={() => Alert.alert("Switched On")}
+        onFalse={() => Alert.alert("Switched Off")}
+      />
     </View>
   );
 }
@@ -15,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
