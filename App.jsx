c666aa1d4b390c2926a4a7757ccb8e6b20735bae
UzairAwanCode <githubprojects000@gmail.com>
2024-05-21 16:08:53 +0500

    Custom Toggle Switches

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { useState } from 'react';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import Toggle from 'react-native-toggle-input'
+
+export default function App() {
+  const [toggle, setToggle] = useState(false)
+  return (
+    <View style={styles.container}>
+      <Toggle toggle={toggle} setToggle={setToggle} color={"orange"} size={20} filled={true} circleColor={"white"} onTrue={()=>Alert.alert("Switched On")} onFalse={()=>Alert.alert("Switched Off")}/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ ReadMe.md
@@ -0,0 +1,2 @@
+# Creating custom toggle switch in react native
+1. install a package npm i react-native-toggle-input
